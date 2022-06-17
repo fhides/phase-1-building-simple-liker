a179f70b7c9730ee7f115f0ef6ec31e933644d68
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-17 14:40:24 +0300

    commit tree 62825504fe73cadd86dd6834f65053c7d46b9472

--- main.js
+++ main.js
@@ -3,7 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal= document.getElementById('modal');
+ const hearts = document.querySelectorAll(".like-glyph");
+ modal.className = "hidden";
+  
+  
+  const likePost = (event) =>{
+    const heart = event.target
+    heart.innerText = EMPTY_HEART
+    mimicServerCall()
+  
+      .then( responce => {
+        (heart.innerText === EMPTY_HEART)? 
+        (heart.innerText = FULL_HEART, heart.classList.add("activated-heart"))
+        :(heart.innerText = EMPTY_HEART, heart.classList.remove("activated-heart")) 
 
+      })
+    
+      .catch(error => {
+        modal.classList.remove("hidden");
+        modal.querySelector("#modal-message").textContent = error;
+        setTimeout( () => {
+          modal.classList.add("hidden")
+        }, 3000)
+      }) 
+
+  }
+  //call event for each love icons
+  hearts.forEach( heart => {
+    heart.addEventListener("click", likePost)
+  })
 
 
 
